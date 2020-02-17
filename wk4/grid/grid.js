c8a70d9e29ca2a6ca77c697ede88c4ed7c38dcc3
hprochnow <57783496+hprochnow@users.noreply.github.com>
2020-02-16 20:54:42 -0600

    Initial Card Game Grid Commit
    Added the canvas and drew the first three game cards.

---
+++ wk4/grid/grid.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(1250, 1250);
+    background(66, 135, 245);
+}
+
+function draw(){
+    rect( 129, 129, 250, 250);
+    fill('yellow');
+    rect( 450, 450, 250, 250);
+    rect( 775, 775, 250, 250);
+}
+
+
+
