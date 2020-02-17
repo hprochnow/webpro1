4f03e9fe88028962c702a6bdb5871d1a8e51432b
hprochnow <57783496+hprochnow@users.noreply.github.com>
2020-02-16 21:01:50 -0600

    Added Remaining Cards to Grid

--- wk4/grid/grid.js
+++ wk4/grid/grid.js
@@ -5,9 +5,16 @@ function setup() {
 
 function draw(){
     rect( 129, 129, 250, 250);
-    fill('yellow');
+    rect( 129, 775, 250, 250);
+    rect( 129, 450, 250, 250);
     rect( 450, 450, 250, 250);
+    rect( 450, 129, 250, 250);
+    rect( 450, 775, 250, 250);
+    rect( 775, 129, 250, 250);
+    rect( 775, 450, 250, 250);
     rect( 775, 775, 250, 250);
+    fill('yellow');
+    noStroke();
 }
 
 
