528e6551dffe18132afe28d92b18c8a0e09991fc
hprochnow <57783496+hprochnow@users.noreply.github.com>
2020-02-20 19:54:59 -0600

    Commit Class w/o Loop Added

---
+++ wk5/class/class.js
@@ -0,0 +1,32 @@
+let bubble;
+
+function setup() {
+    createCanvas(600, 400);
+    bubble = new Bubble();
+}
+
+ function draw() {
+   background(155);
+    bubble.move();
+    bubble.show();
+}
+
+class Bubble {
+    constructor() {
+        this.x = 200;
+        this.y = 150;
+    }
+
+    move() {
+        this.x = this.x + random( -5, 5);
+        this.y = this.y + random( -5, 5);
+    }
+
+    show() {
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        ellipse(this.x, this.y, 24, 24);
+
+    }
+}
