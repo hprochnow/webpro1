c2418f066b8e816bb504c31243db268bb381c0b0
hprochnow <57783496+hprochnow@users.noreply.github.com>
2020-02-23 14:41:52 -0600

    The Card Flip

--- memory/memory.js
+++ memory/memory.js
@@ -1,7 +1,13 @@
+let myCard;
+const DOWN = 'down';
+const UP = 'up';
 function setup() {
     createCanvas(1250, 1250);
     background(66, 135, 245);
-    let myCard = new Card();
+    myCard = new Card();
+}
+function mousePressed() {
+    console.log (myCard.didHit(mouseX, mouseY));
 }
 
 class Card{
@@ -10,11 +16,34 @@ class Card{
         this.y = 129;
         this.width = 200;
         this.height = 200;
+        this.face = DOWN;
         this.show(); 
     }
     show (){        //methods
-        fill('yellow'); 
-        rect (this.x, this.y, this.width, this.height, 10);
+        if(this.face === DOWN) {
+            fill('yellow'); 
+            rect (this.x, this.y, this.width, this.height, 10);    
+        } else {
+            fill ('red');
+            rect (this.x, this.y, this.width, this.height, 10);
+        }
+
+    }
+    didHit (mouseX, mouseY) {
+        if (mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height) {
+                this.flip();
+                return true;
+            } else {
+                return false;
+            }
+    }
+    flip (){
+        if (this.face === DOWN){
+            this.face = UP;
+        } else {
+            this.face = DOWN;
+        }
+        this.show();
     }
 }
 
