e47080abc1463b2ed8ceb3f8a1f1a85550251dbf
hprochnow <57783496+hprochnow@users.noreply.github.com>
2020-03-05 14:14:11 -0600

    Llamas vs Alpacas

---
+++ wk7/lib/script.js
@@ -0,0 +1,29 @@
+let myApp= new Vue ({
+  el: '#myApp',
+  data: {
+    linkAlpaca: 'https://en.wikipedia.org/wiki/Alpaca#Sounds',
+    linkLlama: 'https://en.wikipedia.org/wiki/Llama',
+    llamaOne:'Llamas are much larger than alpacas often weighing up to 400 pounds. Alpacas are smaller, normally weighing between 120 and 150 pounds.',
+    llamaTwo:'Alpacas have straight, pointed ears while Llamas have long banana shaped ears.',
+    llamaThree:'Alpacas are shy herd animals and stick together when threatened by a predator. Llamas are independent and more confident when threatened.',
+    llamaFour:'Llamas and alpacas must regurgitate and re-chew their food to digest it completely.',
+    llamaFive:'Llamas and Alpacas are vegetarians.',
+    llamaSix:'When alpacas are born, the mother and its child hum constantly. They also hum as a sign of distress, especially when they are separated from their herd. Alpacas may also hum if they are curious, happy, worried or cautious.',
+    llamaSeven:'For more than 5,000 years alpacas have been bred for fiber (and in Peru for meat as well), while llamas have been bred for the same amount of time as pack animals and meat.',
+    llamaEight:'Alpacas & llamas are in the camelid family.',
+    llamaNine:'Llamas and Alpacas spit.',
+    llamaTen:'Alpaca has fine, soft fiber, normally 18-30 microns. This fiber is excellent for alpaca garments like shawls, hats and socks. Llama fiber is more coarse, normally 50-65 microns and not as suitable for garments.',
+    seen: true,
+    message:'Llamas and Alpacas spit.',
+    activeColor: 'red',
+    frontSize: '30',
+    headerStyle: {
+      background: 'yellow',
+      border:'5px solid red',
+      margin: '20px',
+      alignment:'center',
+    }
+  },
+
+})
+
