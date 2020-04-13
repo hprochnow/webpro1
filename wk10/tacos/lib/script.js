f570cbf60459e13ea00ebcae93bd7ec695d4ed9b
hprochnow <57783496+hprochnow@users.noreply.github.com>
2020-04-12 21:09:51 -0500

    added a function to try an fix issues

--- wk10/tacos/lib/script.js
+++ wk10/tacos/lib/script.js
@@ -1,25 +1,18 @@
-Vue.component('taco-types', {
-    template:`<div v-bind:class="['taco']"> 
-    <div>{{taco.base_layer}}</div>
-    <div>{{taco.mixin}}</div>
-    <div>{{taco.condiment}}</div>
-    <div>{{taco.shell}}</div>
-    </div>`,
-    props: [`taco`],
-});
-
+var baseURL = "http://taco-randomizer.herokuapp.com"
 const vm = new Vue({
     el: "#tacoApp",
     data: {
-        taco: []
+        tacos: []
     },
-    mounted () {
-        axios
-            .get('http://taco-randomizer.herokuapp.com/random/?full-taco=true')
-            .then(response => {
-                console.log('response', response);
-                vm.tacos = response.data;
-                console.log(vm.tacos);
-            })
+    methods: {
+       getTacos: function () {
+           axios.get(baseURL + "/random/?full-taco=true").then(function(response){
+               console.log(response);
+               vm.tacos = response.data;
+           })
+        }
+    },
+    created: function () {
+        this.getTacos();
     }
-});
+})
